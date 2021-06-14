03d903a9d2e8dfc0c088f3a4013b8c184bd80ee0
Renato <renmazzilli@gmail.com>
2021-06-14 16:47:19 -0500

    Aduld level updated

--- Assets/Scripts/Player/InteractableObjectMovement.cs
+++ Assets/Scripts/Player/InteractableObjectMovement.cs
@@ -13,7 +13,6 @@ public class InteractableObjectMovement : MonoBehaviour
 
     public void StartMoving(Transform player, Vector3 direction)
     {
-
         Quaternion grabbingDirection = Quaternion.identity;
         if(player.rotation.eulerAngles.y < 45 && player.rotation.eulerAngles.y > 315)
         {
@@ -28,16 +27,52 @@ public class InteractableObjectMovement : MonoBehaviour
         {
             grabbingDirection = Quaternion.Euler(player.rotation.eulerAngles.x, 270f, player.rotation.eulerAngles.z);
         }
+        direction.Normalize();
         Vector3 newRotation = grabbingDirection * Vector3.forward;
         newRotation = newRotation * direction.z;
 
-        ObjectMovement(newRotation);
+        // ObjectMovement(newRotation);
+        // ForwardMove(newRotation);
+        
+        if(canMove)
+        {
+            StartCoroutine("MovementForward", newRotation);
+        }
     }
 
+    private float delay = 2f;
     public void ObjectMovement(Vector3 direction)
     {
-        float velocity = 4f;
-        GetComponent<Rigidbody>().position = transform.position + (new Vector3(direction.x, 0, direction.z) * velocity* Time.deltaTime);
+        if(delay < Time.time)
+        {
+            delay += 2f;
+            float velocity = 4f;
+            GetComponent<Rigidbody>().position = transform.position + (new Vector3(direction.x, 0, direction.z) * velocity* Time.deltaTime);
+        }
 
+    }
+    private bool canMove = true;
+    IEnumerator MovementForward(Vector3 direction)
+    {
+        Debug.Log(direction);
+        if(direction.magnitude == 1)
+        {
+            canMove = false;
+            GetComponent<Rigidbody>().AddForce(new Vector3(direction.x, 0, direction.z) * 5, ForceMode.Impulse);
+            Debug.Log("me muevo leches");
+            yield return new WaitForSeconds(1f);
+         
+            canMove = true;
+        }
+        yield return null;
+    }
+
+    public void ForwardMove(Vector3 direction)
+    {
+        if(delay < Time.time)
+        {   
+            delay += Time.time;
+            GetComponent<Rigidbody>().AddForce(direction * 2, ForceMode.Impulse);
+        }
     }
 }
